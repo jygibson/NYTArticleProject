4d6aef5a7d584d28b80102b53558495f16df273f
jygibson <jygibson922@gmail.com>
2018-09-19 21:28:57 -0400

    query and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  $("#search").on("click", function () {
-    var q = $("#search-term").text();
-    var records = $("#records-retrieve").text();
-    var startDate = $("#start-year").text();
-    var endYear = $("#end-year").text();
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    var q = $("#search-term").val();
+    console.log(q)
+    var records = $("#records-retrieve").val();
+    var startDate = $("#start-year").val();
+    var endYear = $("#end-year").val();
     var key = "2175d419e4364743b1efb0ef3243d2ab"
 
 
-    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + "$q=" + q + "$begin_date=" + startDate + "0101" + "$end_date=" + endYear + "0101" + "$api-key" + key;
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + "&q=" + q + "&begin_date=" + startDate + "0101" + "&end_date=" + endYear + "0101" + "&api-key=" + key;
+    console.log(queryUrl);
 
     $.ajax({
       url: queryUrl,
@@ -16,13 +19,13 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response)
 
-      for (var i = 0; i < records; i++)
+      for (var i = 0; i < response.length; i++)
         var div = $("<div>");
-      var snippet = $("<p>").text(response[i].response.docs.snippets);
-      var webUrl = $("<p>").text(response[i].response[i].docs.web_url);
-      var source = $("<p>").text(response[i].response.docs.source);
-      div.append(snippet, webUrl, source);
-      $("#article").append(div)
+      var snippet = $("<p>").text(response.response.docs[i].snippets);
+      var webUrl = $("<p>").text(response.response.docs[i].web_url);
+      var source = $("<p>").text(response.response.docs[i].source);
+      
+      $(".card-body").append(snippet, webUrl, source)
     })
   })
-});
+// });
